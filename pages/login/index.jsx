0655ef27fa64373d8559b4ff10ddbc929c9c2df7
madeby.ariand <92702048+AriandaRiski@users.noreply.github.com>
2023-05-13 17:28:31 +0700

    buat akun menggunakan akun gmail (#5)

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,13 +1,15 @@
 import Login from '@/components/pendaftaran/login'
 import Layout from '@/layouts/Layout'
+import { useSession } from 'next-auth/react'
 import React from 'react'
+import { useState } from 'react';
 
 export default function pageLogin() {
   return (
     <>
-        <Layout title="Login">
-            <Login/>
-        </Layout>
+      <Layout title="Login">
+        <Login />
+      </Layout>
     </>
   )
 }
